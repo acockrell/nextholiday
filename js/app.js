1bdd75de4520119d6bfd5c2c0a91ff93dc96ead5
Gavin McDonald <gmcdonald@ea.com>
2018-01-05 10:25:49 -0800

    borked the merge, fixing.

--- js/app.js
+++ js/app.js
@@ -1,71 +1,4 @@
 define([
-<<<<<<< HEAD
-    'underscore',
-    'backbone',
-    'views/HolidaysView',
-    'collections/Holidays'
-], function (_, Backbone, HolidaysView, Holidays) {
-    var initialize = function() {
-        var holidays = new Holidays([{
-            description: 'New Year\'s Day',
-            date: 'January 2, 2017'
-        }, {
-            description: 'Family Day',
-            date: 'February 13, 2017'
-        }, {
-            description: 'Good Friday',
-            date: 'April 14, 2017'
-        }, {
-            description: 'Victoria Day',
-            date: 'May 22, 2017'
-        }, {
-            description: 'Mitchell\'s Birthday',
-            date: 'June 15, 2017',
-            image: 'birthday.jpg'
-        }, {
-            description: 'Canada Day',
-            date: 'July 3, 2017'
-        }, {
-            description: 'BC Day',
-            date: 'August 7, 2017'
-        }, {
-            description: 'Labour Day',
-            date: 'September 4, 2017'
-        }, {
-            description: 'Thanksgiving Day',
-            date: 'October 9, 2017',
-            image: 'thanksgiving.jpg'
-        }, {
-            description: 'Remembrance Day',
-            date: 'November 13, 2017',
-            image: 'veterans-day.jpg'
-        }, {
-            description: 'Christmas Day',
-            date: 'December 25, 2015',
-            image: 'xmas.jpg'
-        }, {
-            description: 'Boxing Day',
-            date: 'December 26, 2015',
-            image: 'xmas.jpg'
-        }, {
-            description: 'XMAS Closing',
-            date: 'December 27, 2017',
-            image: 'xmas.jpg'
-        }, {
-            description: 'XMAS Closing',
-            date: 'December 28, 2017',
-            image: 'xmas.jpg'
-        }, {
-            description: 'XMAS Closing',
-            date: 'December 29, 2017',
-            image: 'xmas.jpg'
-        }, {
-            description: 'XMAS Closing',
-            date: 'December 30, 2017',
-            image: 'xmas.jpg'
-        }
-    ]);
-=======
 	'underscore',
 	'backbone',
 	'router',
@@ -75,7 +8,6 @@ define([
 	var initialize = function() {
 		Router.initialize();
 	};
->>>>>>> upstream/master
 
 	return {
 		initialize: initialize
